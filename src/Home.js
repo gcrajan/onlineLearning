ff1f77a165c8f255f3bf83516eb8eda181bfe58b
Rajan GC <gcrajan339@gmail.com>
2022-07-01 13:02:38 +0545

    test

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 import Header from './Component/header/Header';
 import Section from './Component/header/Section';
 import Footer from './Component/footer/Footer';
-import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
     return ( <div>
